ee0d74acac8126bc7b4668a72a0019f68f3cf7cb
Jacques <jack.polart@gmail.com>
2021-06-28 09:09:07 +0200

    fix: routes name for blog

--- gatsby/pagination/create-posts-pages.js
+++ gatsby/pagination/create-posts-pages.js
@@ -19,13 +19,13 @@ module.exports = async (graphql, actions) => {
 
   for (let i = 0; i < numPages; i += 1) {
     createPage({
-      path: i === 0 ? '/blog' : `/page/${i}`,
+      path: i === 0 ? '/blog' : `/blog/${i}`,
       component: path.resolve('./src/templates/index-template.js'),
       context: {
         currentPage: i,
         postsLimit: postsPerPage,
         postsOffset: i * postsPerPage,
-        prevPagePath: i <= 1 ? '/blog' : `/page/${i - 1}`,
+        prevPagePath: i <= 1 ? '/blog' : `/blog/${i - 1}`,
         nextPagePath: `/page/${i + 1}`,
         hasPrevPage: i !== 0,
         hasNextPage: i !== numPages - 1
