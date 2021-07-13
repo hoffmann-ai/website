71f8181a5a9eccec6c9ace80941290f490517c7a
Jacques <jack.polart@gmail.com>
2021-07-13 15:18:15 +0200

    feat: add last posts module

--- src/templates/landing-template.js
+++ src/templates/landing-template.js
@@ -3,13 +3,26 @@ import React from 'react';
 import Layout from '../components/Layout';
 import Page from '../components/Page';
 import Landing from '../components/Landing/Landing';
+import type { AllMarkdownRemark } from '../types';
 
-const LandingTemplate = () => (
-  <Layout title={'Accueil - HOFFMANN.AI'}>
-    <Page>
-      <Landing />
-    </Page>
-  </Layout>
-);
+type Props = {
+  pageContext: {
+    lastPosts: {
+      data: AllMarkdownRemark
+    }
+  }
+}
+
+const LandingTemplate = ({ pageContext }: Props) => {
+  const lastPosts = pageContext.lastPosts.data.allMarkdownRemark.edges;
+
+  return (
+    <Layout title={'Accueil - HOFFMANN.AI'}>
+      <Page>
+        <Landing lastPosts={lastPosts} />
+      </Page>
+    </Layout>
+  );
+};
 
 export default LandingTemplate;
