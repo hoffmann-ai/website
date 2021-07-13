099fbb919d22d311eebaf1cab82d118362b8bf34
Jacques <jack.polart@gmail.com>
2021-07-13 15:18:15 +0200

    feat: add last posts module

---
+++ src/components/Landing/LastPosts/LastPosts.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link, navigate } from 'gatsby';
+import styles from './LastPosts.module.scss';
+
+const LastPosts = ({ lastPosts }) => {
+  console.log(lastPosts);
+
+  return (
+    <div className={styles['container']}>
+      {lastPosts.map((post, i) => (
+        <div key={i} className={styles['card']}>
+          <p className={styles['category']}>{post.node.frontmatter.category}</p>
+          <img
+            src={post.node.frontmatter.socialImage.childImageSharp.fluid.srcWebp}
+            onClick={() => navigate(post.node.fields.slug)}/>
+          <h4 onClick={() => navigate(post.node.fields.slug)}>{post.node.frontmatter.title}</h4>
+          <p>{post.node.frontmatter.description}</p>
+          <Link to={post.node.fields.slug} className={styles['button']}>voir l'article</Link>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default LastPosts;
