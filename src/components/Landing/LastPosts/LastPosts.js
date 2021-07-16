65ddb34ae9ad306e728cf0e4e1a8e34ccadcccd9
Jacques <jack.polart@gmail.com>
2021-07-16 12:10:33 +0200

    fix: last post : set categorie to uppercase, install and use gatsby-image

--- src/components/Landing/LastPosts/LastPosts.js
+++ src/components/Landing/LastPosts/LastPosts.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Link, navigate } from 'gatsby';
+import Img from 'gatsby-image';
 import styles from './LastPosts.module.scss';
 
 const LastPosts = ({ lastPosts }) => (
     <div className={styles['container']}>
       {lastPosts.map((post, i) => (
         <div key={i} className={styles['card']}>
-          <p className={styles['category']}>{post.node.frontmatter.category}</p>
-          <img
-            src={post.node.frontmatter.socialImage.childImageSharp.fluid.srcWebp}
+          <p className={styles['category']}>{post.node.frontmatter.category.toUpperCase()}</p>
+          <Img
+            fluid={post.node.frontmatter.socialImage.childImageSharp.fluid}
             onClick={() => navigate(post.node.fields.slug)}/>
           <h4 onClick={() => navigate(post.node.fields.slug)}>{post.node.frontmatter.title}</h4>
           <p>{post.node.frontmatter.description}</p>
