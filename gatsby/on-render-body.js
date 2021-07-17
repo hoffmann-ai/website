fea85d62a99a09eaa0740854d747001949070f52
Alexander Hoffmann <alexander@hoffmann.ai>
2021-07-17 11:21:11 +0200

    feat: implement cookies (#29)
    * feat: implement cookies
    
    * fix: lint errors

--- gatsby/on-render-body.js
+++ gatsby/on-render-body.js
@@ -6,17 +6,37 @@ const siteConfig = require('../config.js');
 // eslint-disable-next-line import/no-webpack-loader-syntax, import/no-unresolved
 const katexStylesheet = require('!css-loader!../static/css/katex/katex.min.css');
 
-const onRenderBody = ({ setHeadComponents }) => {
+const onRenderBody = ({ setHeadComponents, setPostBodyComponents }) => {
   const { useKatex } = siteConfig;
 
   if (useKatex) {
     setHeadComponents([
       React.createElement('style', {
         key: 'katex-inline-stylesheet',
-        dangerouslySetInnerHTML: { __html: katexStylesheet.toString() }
-      })
+        dangerouslySetInnerHTML: { __html: katexStylesheet.toString() },
+      }),
     ]);
   }
+
+  setPostBodyComponents([
+    <script
+      key="axeptio"
+      dangerouslySetInnerHTML={{
+        __html: `
+          window.axeptioSettings = {
+            clientId: "60f291c2afa9d02a7f02768e",
+            cookiesVersion: "hoffmann.ai-base",
+          };
+          (function(d, s) {
+            var t = d.getElementsByTagName(s)[0], e = d.createElement(s);
+            e.async = true;
+            e.src = "//static.axept.io/sdk-slim.js";
+            t.parentNode.insertBefore(e, t);
+          })(document, "script");
+      `,
+      }}
+    />,
+  ]);
 };
 
 module.exports = onRenderBody;
