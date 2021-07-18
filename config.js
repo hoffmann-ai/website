6b0b3cfbfd5f478e271abe70a2c2bd5e733d2163
Alexander Hoffmann <alexander@hoffmann.ai>
2021-07-18 14:17:53 +0200

    refactor: remove disqus

--- config.js
+++ config.js
@@ -6,7 +6,6 @@ module.exports = {
   title: 'HOFFMANN.AI',
   subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
   copyright: '© All rights reserved.',
-  disqusShortname: '',
   postsPerPage: 4,
   googleAnalyticsId: 'UA-73379983-2',
   useKatex: false,
