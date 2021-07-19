e47cc8c5d745edf8f769199b2a8506ecfc5c99ea
Jacques <jack.polart@gmail.com>
2021-07-19 14:55:38 +0200

    fix: skills images, check if value existe before displaying it

--- src/components/Landing/skills/skills.js
+++ src/components/Landing/skills/skills.js
@@ -8,12 +8,12 @@ const Skills = ({ skills }) => (
       <h3>Notre expertise</h3>
       <p>pour rester à la pointe de la technologie</p>
     </div>
-    <GatsbyImage image={skills[0].node.childImageSharp.gatsbyImageData} alt="aws" className={styles['logo']} style={{ top: '20%', left: '30%' }}/>
-    <GatsbyImage image={skills[1].node.childImageSharp.gatsbyImageData} alt="azure" className={styles['logo']} style={{ top: '20%', left: '50%' }}/>
-    <GatsbyImage image={skills[2].node.childImageSharp.gatsbyImageData} alt="cloud" className={styles['logo']} style={{ top: '20%', left: '70%' }}/>
-    <GatsbyImage image={skills[3].node.childImageSharp.gatsbyImageData} alt="docker" className={styles['logo']} style={{ top: '80%', left: '30%' }}/>
-    <GatsbyImage image={skills[4].node.childImageSharp.gatsbyImageData} alt="kubernetes" className={styles['logo']} style={{ top: '80%', left: '50%' }}/>
-    <GatsbyImage image={skills[5].node.childImageSharp.gatsbyImageData} alt="prometheus" className={styles['logo']} style={{ top: '80%', left: '70%' }}/>
+    {skills[0] && <GatsbyImage image={skills[0].node.childImageSharp.gatsbyImageData} alt="aws" className={styles['logo']} style={{ top: '20%', left: '30%' }}/>}
+    {skills[1] && <GatsbyImage image={skills[1].node.childImageSharp.gatsbyImageData} alt="azure" className={styles['logo']} style={{ top: '20%', left: '50%' }}/>}
+    {skills[2] && <GatsbyImage image={skills[2].node.childImageSharp.gatsbyImageData} alt="cloud" className={styles['logo']} style={{ top: '20%', left: '70%' }}/>}
+    {skills[3] && <GatsbyImage image={skills[3].node.childImageSharp.gatsbyImageData} alt="docker" className={styles['logo']} style={{ top: '80%', left: '30%' }}/>}
+    {skills[4] && <GatsbyImage image={skills[4].node.childImageSharp.gatsbyImageData} alt="kubernetes" className={styles['logo']} style={{ top: '80%', left: '50%' }}/>}
+    {skills[5] && <GatsbyImage image={skills[5].node.childImageSharp.gatsbyImageData} alt="prometheus" className={styles['logo']} style={{ top: '80%', left: '70%' }}/>}
   </div>
 );
 
