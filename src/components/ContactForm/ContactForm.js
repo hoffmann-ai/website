2e35020e396293e408f86ef81de62065659e3c27
Eliott Morcillo <eliott.morcillo@gmail.com>
2021-07-20 13:36:05 +0200

    removed useless empty field checks

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -4,36 +4,12 @@ import addToMailchimp from 'gatsby-plugin-mailchimp';
 import styles from './ContactForm.module.scss';
 
 const ContactForm = () => {
-  // variables
-  const [name, setName] = useState('');
-  const [firstname, setFirstname] = useState('');
-  const [phone, setPhone] = useState('');
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
-  const [submit, setSubmit] = useState(false);
   const [newsletterAgree, setSewsletterAgree] = useState(false);
+  const [email, setEmail] = useState('');
   const handleClickCheckBox = () => setSewsletterAgree(!newsletterAgree);
 
-  // empty inputs messages
-  const error = 'Ce champ est requis.';
-
-  const handleSubmit = (e) => {
-    setSubmit(true);
-    if (email.length !== 0 && name.length !== 0 && firstname.length !== 0) {
-      sendFeedback();
-    } else {
-      e.preventDefault();
-    }
-  };
-
-  // reeceives templateId, variables in parameters
   const sendFeedback = () => {
     if (newsletterAgree) addToMailchimp(email);
-    setName('');
-    setFirstname('');
-    setPhone('');
-    setEmail('');
-    setMessage('');
   };
 
   return (
@@ -52,34 +28,26 @@ const ContactForm = () => {
           <label className={styles['form__inner__grid__element__child']}>
             Prénom
             <span className={styles['form__inner__star']}> *</span>
-            <span className={styles['form__inner__error__message']}>
-              {firstname.length === 0 && submit ? error : ''}
-            </span>
           </label>
           <input
             type='text'
             className={styles['form__inner__grid__element__child']}
             name='name'
-            onChange={(e) => setFirstname(e.target.value)}
             placeholder='Saisir votre prénom'
-            value={firstname}
             autoComplete='off'
+            required
           />
         </div>
         <div className={styles['form__inner__grid__element']}>
           <label className={styles['form__inner__grid__element__child']}>
             Nom <span className={styles['form__inner__star']}> *</span>
-            <span className={styles['form__inner__error__message']}>
-              {name.length === 0 && submit ? error : ''}
-            </span>
           </label>
           <input
             type='text'
             className={styles['form__inner__grid__element__child']}
             name='firstname'
-            onChange={(e) => setName(e.target.value)}
             placeholder='Saisir votre Nom'
-            value={name}
+            required
           />
         </div>
         <div className={styles['form__inner__grid__element']}>
@@ -90,17 +58,12 @@ const ContactForm = () => {
             type='text'
             className={styles['form__inner__grid__element__child']}
             name='phone'
-            onChange={(e) => setPhone(e.target.value)}
             placeholder='ex : 06 32 32 32 32'
-            value={phone}
           />
         </div>
         <div className={styles['form__inner__grid__element']}>
           <label className={styles['form__inner__grid__element__child']}>
             Email <span className={styles['form__inner__star']}> *</span>
-            <span className={styles['form__inner__error__message']}>
-              {email.length === 0 && submit ? error : ''}
-            </span>
           </label>
           <label className={styles['form__not-email']}>Email non valide</label>
           <input
@@ -109,8 +72,8 @@ const ContactForm = () => {
             name='email'
             onChange={(e) => setEmail(e.target.value)}
             placeholder='email *'
-            value={email}
             autoComplete='off'
+            required
           />
         </div>
       </div>
@@ -121,9 +84,7 @@ const ContactForm = () => {
         <textarea
           className={styles['form__inner__grid__element__child']}
           name='message'
-          onChange={(e) => setMessage(e.target.value)}
           placeholder='Saisir votre message'
-          value={message}
         />
       </div>
       <div className={styles['form__inner__grid__element__terms']}>
@@ -147,8 +108,8 @@ const ContactForm = () => {
         <input
           className={styles['form__input']}
           type='submit'
+          onClick={sendFeedback}
           value='Envoyer'
-          onClick={handleSubmit}
         />
         <div className={styles['form__message']}></div>
       </div>
