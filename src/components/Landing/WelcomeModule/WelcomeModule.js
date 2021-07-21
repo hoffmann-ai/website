de6cd5e2af9279af70ccf3ecd5dff93cd10f2c0b
Eliott Morcillo <eliott.morcillo@gmail.com>
2021-07-21 17:52:37 +0200

    checked if array not empty fix

--- src/components/Landing/WelcomeModule/WelcomeModule.js
+++ src/components/Landing/WelcomeModule/WelcomeModule.js
@@ -4,18 +4,22 @@ import styles from './WelcomeModule.module.scss';
 
 const WelcomeModule = ({ landing }) => (
   <div className={styles['welcome__module']}>
-    <GatsbyImage
-      image={landing[0].node.childImageSharp.gatsbyImageData}
-      height={landing[0].node.childImageSharp.gatsbyImageData.height}
-      width={landing[0].node.childImageSharp.gatsbyImageData.width}
-      alt={landing[0].node.name}
-      key={landing[0].node.name}
-    />
-    <h1 className={styles['welcome__module__title']}>HOFFMANN.AI</h1>
-    <h6 className={styles['welcome__module__text']}>
-      Une Agence spécialisée dans la digitalisation, qui met son expertise au
-      service des particuliers et professionnels évoluant en France.
-    </h6>
+    {landing[0] && (
+      <>
+        <GatsbyImage
+          image={landing[0].node.childImageSharp.gatsbyImageData}
+          height={landing[0].node.childImageSharp.gatsbyImageData.height}
+          width={landing[0].node.childImageSharp.gatsbyImageData.width}
+          alt={landing[0].node.name}
+          key={landing[0].node.name}
+        />
+        <h1 className={styles['welcome__module__title']}>HOFFMANN.AI</h1>
+        <h6 className={styles['welcome__module__text']}>
+          Une Agence spécialisée dans la digitalisation, qui met son expertise
+          au service des particuliers et professionnels évoluant en France.
+        </h6>
+      </>
+    )}
   </div>
 );
 
