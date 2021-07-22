eef0828135a480aed47d67e23d4ee63505e10cb5
Jacques <jack.polart@gmail.com>
2021-07-22 09:17:58 +0200

    fix: refactor

--- src/components/Landing/HeroModule/HeroModule.js
+++ src/components/Landing/HeroModule/HeroModule.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { GatsbyImage } from 'gatsby-plugin-image';
-import styles from './WelcomeModule.module.scss';
+import styles from './HeroModule.module.scss';
 
-const WelcomeModule = ({ landing }) => (
-  <div className={styles['welcome__module']}>
+const Hero = ({ landing }) => (
+  <div className={styles['hero__module']}>
     {landing[0] && (
       <>
         <GatsbyImage
@@ -13,8 +13,8 @@ const WelcomeModule = ({ landing }) => (
           alt={landing[0].node.name}
           key={landing[0].node.name}
         />
-        <h1 className={styles['welcome__module__title']}>HOFFMANN.AI</h1>
-        <h6 className={styles['welcome__module__text']}>
+        <h1 className={styles['hero__module__title']}>HOFFMANN.AI</h1>
+        <h6 className={styles['hero__module__text']}>
           Une Agence spécialisée dans la digitalisation, qui met son expertise
           au service des particuliers et professionnels évoluant en France.
         </h6>
@@ -23,4 +23,4 @@ const WelcomeModule = ({ landing }) => (
   </div>
 );
 
-export default WelcomeModule;
+export default Hero;
--- src/components/Landing/LastPosts/LastPosts.js
+++ src/components/Landing/LastPosts/LastPosts.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, navigate } from 'gatsby';
+import { Link } from 'gatsby';
 import { GatsbyImage } from 'gatsby-plugin-image';
 import styles from './LastPosts.module.scss';
 
@@ -7,17 +7,18 @@ const LastPosts = ({ lastPosts }) => (
   <>
     <h1>Nos derniers posts</h1>
     <div className={styles['container']}>
-      {lastPosts.map((post, i) => (
-        <div key={i} className={styles['card']}>
+      {lastPosts.map((post) => (
+        <Link to={post.node.fields.slug} key={post.node.fields.slug} className={styles['card']}>
           <p className={styles['category']}>{post.node.frontmatter.category.toUpperCase()}</p>
           <GatsbyImage
             image={post.node.frontmatter.socialImage.childImageSharp.gatsbyImageData}
             alt={post.node.frontmatter.title}
-            onClick={() => navigate(post.node.fields.slug)}/>
-          <h4 onClick={() => navigate(post.node.fields.slug)}>{post.node.frontmatter.title}</h4>
+            height={post.node.frontmatter.socialImage.childImageSharp.gatsbyImageData.height}
+            width={post.node.frontmatter.socialImage.childImageSharp.gatsbyImageData.width}/>
+          <h4>{post.node.frontmatter.title}</h4>
           <p>{post.node.frontmatter.description}</p>
-          <Link to={post.node.fields.slug} className={styles['button']}>voir l'article</Link>
-        </div>
+          <button className={styles['button']}>Voir l'article</button>
+        </Link>
       ))}
     </div>
   </>
