b4138cb2193e8459bf4b1f66e9e1325ff0795a1e
Jacques <jack.polart@gmail.com>
2021-07-23 14:11:44 +0200

    fix: use gatsbyimage in navbar and footer

--- src/components/Footer/MiniLogo.js
+++ src/components/Footer/MiniLogo.js
@@ -1,24 +1,26 @@
 // @flow strict
 import React from 'react';
-import { withPrefix, Link } from 'gatsby';
+import { Link } from 'gatsby';
+import { GatsbyImage } from 'gatsby-plugin-image';
 import styles from './MiniLogo.module.scss';
+import { useGlobalData } from '../../hooks';
 
-type Props = {
-  img: string,
+const MiniLogo = () => {
+  const { logo } = useGlobalData();
+
+  return (
+    <div className={styles['logo']}>
+      <Link to="/">
+        <GatsbyImage
+          image={logo?.childImageSharp?.gatsbyImageData}
+          className={styles['logo__photo']}
+          width="30"
+          height="30"
+          alt="logo"
+        />
+      </Link>
+    </div>
+  );
 };
-
-const MiniLogo = ({ img }: Props) => (
-  <div className={styles['logo']}>
-    <Link to="/">
-      <img
-        src={withPrefix(img)}
-        className={styles['logo__photo']}
-        width="30"
-        height="30"
-        alt="logo"
-      />
-    </Link>
-  </div>
-);
 
 export default MiniLogo;
