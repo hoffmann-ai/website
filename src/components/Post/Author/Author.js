9eed98e1d4d70ad249a78cd0b81316adaf6d7625
Alexander Hoffmann <alexander@hoffmann.ai>
2021-07-24 09:21:33 +0200

    article: mailchimp

--- src/components/Post/Author/Author.js
+++ src/components/Post/Author/Author.js
@@ -1,6 +1,5 @@
 // @flow strict
 import React from 'react';
-import { getContactHref } from '../../../utils';
 import styles from './Author.module.scss';
 import { useSiteMetadata } from '../../../hooks';
 
@@ -10,15 +9,9 @@ const Author = () => {
   return (
     <div className={styles['author']}>
       <p className={styles['author__bio']}>
+        <strong>{author.name}</strong>
+        <br />
         {author.bio}
-        <a
-          className={styles['author__bio-twitter']}
-          href={getContactHref('twitter', author.contacts.twitter)}
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          <strong>{author.name}</strong> on Twitter
-        </a>
       </p>
     </div>
   );
