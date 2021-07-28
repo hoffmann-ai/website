5a6e6efdc80de6ce8fb4544138850581c38ecbb9
Jacques <jack.polart@gmail.com>
2021-07-28 17:07:03 +0200

    refractor module design contruction

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -14,7 +14,6 @@ const Landing = ({
     <Hero landing={landing} />
     <CallToAction />
     <div className={styles['services']}>
-      <h2>Nos services</h2>
       <Services services={services} />
     </div>
     <Clients clients={clients} />
