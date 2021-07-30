8a6bdcfedee2225189ecc7e8b42230d4da94f9da
Eliott Morcillo <eliott.morcillo@gmail.com>
2021-07-30 14:28:25 +0200

    feat: new services module (responsive)

--- src/components/Landing/Services/Services.js
+++ src/components/Landing/Services/Services.js
@@ -1,76 +1,49 @@
-// @flow strict
 import React from 'react';
 import { GatsbyImage, gatsbyImageData } from 'gatsby-plugin-image';
+import CallToAction from '../../CallToAction';
 import styles from './Services.module.scss';
 
 type Props = {
   node: {
-    childImageSharp: typeof gatsbyImageData,
-    name: string
+    frontmatter: {
+      name: string,
+      description: string,
+      logo: {
+        childImageSharp: {
+          gatsbyImageData: typeof gatsbyImageData
+        }
+      }
+    }
   }
-}
-
-const Services = ({ services } : { services : Array<Props> }) => (
-  <div className={styles['services__module']}>
+};
+const Services = ({ services }: { services: Array<Props> }) => (
+  <div className={styles['container_serv']}>
     <h2>Nos services</h2>
-    <p className={styles['text_light']}>Les services dans lesquels nous sommes experts</p>
-    <div className={styles['services__module__innergrid']}>
-      <div className={styles['services__module__innergrid__comp']}>
-        {services[1] && (
-          <>
-            <GatsbyImage
-              image={services[1].node.childImageSharp.gatsbyImageData}
-              height={services[1].node.childImageSharp.gatsbyImageData.height}
-              width={services[1].node.childImageSharp.gatsbyImageData.width}
-              alt={services[1].node.name}
-              key={services[1].node.name}
-              className={styles['services__module__innergrid__comp__image']}
-            />
-            <h4>Cloud</h4>
-            <p className={styles['text_light']}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
-              vestibulum aliquam metus.
-            </p>
-          </>
-        )}
+    <h6 className={styles['text_light']}>les services dans lesquels nous sommes experts</h6>
+    {services.map((service) => (
+      <div className={styles['service_container']}>
+        <div className={styles['service_container_content']}>
+          <GatsbyImage
+            image={service.node.frontmatter.logo.childImageSharp.gatsbyImageData}
+            height={service.node.frontmatter.logo.childImageSharp.gatsbyImageData.height}
+            width={service.node.frontmatter.logo.childImageSharp.gatsbyImageData.width}
+            alt={service.node.frontmatter.name}
+            key={service.node.frontmatter.name}
+            className={styles['service_container_image']}
+          />
+        </div>
+        <div className={styles['service_container_content']}>
+          <h4 className={styles['service_container_content_title']}>
+            {service.node.frontmatter.name}
+          </h4>
+          <p className={styles['service_container_content_text']}>
+            {service.node.frontmatter.description}
+          </p>
+          <CallToAction />
+        </div>
       </div>
-      <div className={styles['services__module__innergrid__comp']}>
-        {services[0] && (
-          <>
-            <GatsbyImage
-              image={services[0].node.childImageSharp.gatsbyImageData}
-              height={services[0].node.childImageSharp.gatsbyImageData.height}
-              width={services[0].node.childImageSharp.gatsbyImageData.width}
-              alt={services[0].node.name}
-              key={services[0].node.name}
-              className={styles['services__module__innergrid__comp__image']}
-            />
-            <h4>DevOps & InfraOps</h4>
-            <p className={styles['text_light']}>
-              Ansible, Docker, Git, Kubernetes, Maven, Unix, Vagrant
-            </p>
-          </>
-        )}
-      </div>
-      <div className={styles['services__module__innergrid__comp']}>
-        {services[2] && (
-          <>
-            <GatsbyImage
-              image={services[2].node.childImageSharp.gatsbyImageData}
-              height={services[2].node.childImageSharp.gatsbyImageData.height}
-              width={services[2].node.childImageSharp.gatsbyImageData.width}
-              alt={services[2].node.name}
-              key={services[2].node.name}
-              className={styles['services__module__innergrid__comp__image']}
-            />
-            <h4>Technologies Web</h4>
-            <p className={styles['text_light']}>
-              AWS, CSS, HTML, Java Spring, NodeJS, ReactJS, Shopify, Wordpress
-            </p>
-          </>
-        )}
-      </div>
-    </div>
+    ))}
   </div>
 );
+
 export default Services;
