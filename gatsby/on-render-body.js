356a7063172ed5a3c512d5f46651d11df3aa3afa
Alexander Hoffmann <alexander@hoffmann.ai>
2021-10-20 17:51:57 +0200

    refactor: remove external js (#36)

--- gatsby/on-render-body.js
+++ gatsby/on-render-body.js
@@ -1,27 +1,6 @@
 'use strict';
 
-const React = require('react');
-
-const onRenderBody = ({ setPostBodyComponents }) => {
-  setPostBodyComponents([
-    <script
-      key="axeptio"
-      dangerouslySetInnerHTML={{
-        __html: `
-          window.axeptioSettings = {
-            clientId: "60f291c2afa9d02a7f02768e",
-            cookiesVersion: "hoffmann.ai-base",
-          };
-          (function(d, s) {
-            var t = d.getElementsByTagName(s)[0], e = d.createElement(s);
-            e.async = true;
-            e.src = "//static.axept.io/sdk-slim.js";
-            t.parentNode.insertBefore(e, t);
-          })(document, "script");
-      `,
-      }}
-    />
-  ]);
+const onRenderBody = () => {
 };
 
 module.exports = onRenderBody;
